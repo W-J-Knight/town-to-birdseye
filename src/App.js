bad61662611ad1fb07b957ce4ee5eb9576826c1f
William <William.Jeffery.Knight@protonmail.com>
2022-08-03 08:26:12 -0400

    fix the display of birdseye svg files go to /node_modules/@babel/preset-react/lib/index.js
    find the normalizeOptions function
    then look for throwIfNamespace
    then set throwIfNamespace to false
    
    restart the app

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
 import React from 'react';
 import './App.css';
 import { Container, Navbar, NavbarBrand } from 'reactstrap';
+import Birdseye from './app/asset/images/Birdseye-2.svg';
+
+
 
 function App() {
-  return (
-    <div className="App">
+    return (
+        <div className='App'>
             <Navbar dark color='primary' sticky='top' expand='md'>
                 <Container>
                     <NavbarBrand href='/'>
-                        <h1>Birdseye</h1>
+                        <img src={Birdseye} alt='birdsye logo' />
                     </NavbarBrand>
                 </Container>
             </Navbar>
             I'm ready for workshop!
-    </div>
-  );
+        </div>
+    );
 }
+
+
 
 export default App;
